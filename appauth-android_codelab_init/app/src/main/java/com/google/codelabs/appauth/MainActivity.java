5818f7ff0c8e30e935830c8448ee25e4bcf64e60
Kelli Davis <keldavis5@yahoo.com>
2019-02-22 03:49:00 -0500

    feat: Add Android for Work Managed Configuration - create the schema - add reference to schema in manifest - retrieve configurations once set

--- appauth-android_codelab_init/app/src/main/java/com/google/codelabs/appauth/MainActivity.java
+++ appauth-android_codelab_init/app/src/main/java/com/google/codelabs/appauth/MainActivity.java
@@ -15,12 +15,16 @@
 package com.google.codelabs.appauth;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.RestrictionsManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.UserManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -32,6 +36,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -60,7 +65,10 @@ public class MainActivity extends AppCompatActivity {
 
   MainApplication mMainApplication;
 
-  // state
+  private final static String LOGIN_HINT = "login_hint";
+
+
+    // state
   AuthState mAuthState;
 
   // views
@@ -71,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
   AppCompatTextView mFamilyName;
   AppCompatTextView mFullName;
   ImageView mProfileView;
+  // login hint;
+  String mLoginHint;
+
+  // broadcast receiver for app restrictions changed broadcast
+  private BroadcastReceiver mRestrictionsReceiver;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -89,6 +102,9 @@ public class MainActivity extends AppCompatActivity {
 
     // wire click listeners
     mAuthorize.setOnClickListener(new AuthorizeListener());
+
+      // Retrieve app restrictions and take appropriate action
+      getAppRestrictions();
   }
 
   private void enablePostAuthorizationFlows() {
@@ -228,6 +244,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         checkIntent(getIntent());
+        // Register a receiver for app restrictions changed broadcast
+        registerRestrictionsReceiver();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        // Retrieve app restrictions and take appropriate action
+        getAppRestrictions();
+
+        // Register a receiver for app restrictions changed broadcast
+        registerRestrictionsReceiver();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // Unregister receiver for app restrictions changed broadcast
+        unregisterReceiver(mRestrictionsReceiver);
     }
 
     public static class SignOutListener implements Button.OnClickListener {
@@ -323,4 +360,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
   }
+
+    private void getAppRestrictions(){
+        RestrictionsManager restrictionsManager =
+                (RestrictionsManager) this
+                        .getSystemService(Context.RESTRICTIONS_SERVICE);
+
+        Bundle appRestrictions = restrictionsManager.getApplicationRestrictions();
+
+        if(!appRestrictions.isEmpty()){
+            if(appRestrictions.getBoolean(UserManager.
+                    KEY_RESTRICTIONS_PENDING)!=true){
+                mLoginHint = appRestrictions.getString(LOGIN_HINT);
+            }
+            else {
+                Toast.makeText(this,R.string.restrictions_pending_block_user,
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    private void registerRestrictionsReceiver(){
+        IntentFilter restrictionsFilter =
+                new IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED);
+
+        mRestrictionsReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                getAppRestrictions();
+            }
+        };
+
+        registerReceiver(mRestrictionsReceiver, restrictionsFilter);
+    }
+
+
 }
